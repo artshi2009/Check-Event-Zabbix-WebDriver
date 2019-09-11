d7ea2713d8df93180e5051deb7791a2b9d03cb24
Arthur <artshi2009@gmail.com>
2019-09-11 18:45:55 +0300

    search fix item per page

--- src/main/java/ru/spaces/artshi2009/ActionWeb.java
+++ src/main/java/ru/spaces/artshi2009/ActionWeb.java
@@ -1,28 +1,37 @@
 package ru.spaces.artshi2009;
 
 import com.google.common.base.Throwables;
-import io.github.bonigarcia.wdm.ChromeDriverManager;
+import io.github.bonigarcia.wdm.DriverManagerType;
+import io.github.bonigarcia.wdm.FirefoxDriverManager;
 import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
-import static io.github.bonigarcia.wdm.DriverManagerType.CHROME;
+import java.util.ArrayList;
+import java.util.List;
 
 
 class ActionWeb {
-    private LogFile log = new LogFile();
-    private int reconnectTry = 1;
+    private WebDriver driver;
+    private LogFile log;
+    private int reconnectTry;
+
+    ActionWeb(){
+        log = new LogFile();
+        reconnectTry = 1;
+    }
 
     void RunActionWeb(){
-        ChromeDriverManager.getInstance(CHROME).setup();
-        WebDriver driver = new ChromeDriver();
+        FirefoxDriverManager.getInstance(DriverManagerType.FIREFOX).setup();
+        driver = new FirefoxDriver(new FirefoxOptions().setHeadless(true));
 
         for (int i = 1; i <= reconnectTry; i++) {
             try {
                 driver.get(Params.getLoginPage());
-                loginZabbix(driver, 5000, Params.getLogin(), Params.getPassword());
+                loginZabbix(driver, 3000, Params.getLogin(), Params.getPassword());
                 driver.get(Params.getDashboardPage());
-                searchAlert(driver, 3000);
-                closingAlert(driver, 3000);
+                searchAlert(driver, 2000);
+                closingAlert(driver, 2000);
             } catch (NoSuchElementException | InterruptedException ex) {
                 log.logFile("Не обнаружен элемент при поиске на странице: \n" +
                         Throwables.getStackTraceAsString(ex));
@@ -36,10 +45,10 @@ class ActionWeb {
                 log.logFile("Непредвиденная ошибка: \n" +
                         Throwables.getStackTraceAsString(ex));
                 ex.printStackTrace();
+            } finally {
+                driver.quit();
             }
         }
-
-        driver.quit();
     }
 
     private void closingAlert(WebDriver driver, int waitRun) throws InterruptedException {
@@ -50,21 +59,33 @@ class ActionWeb {
             driver.findElement(By.id("acknowledge_problem")).click();
             driver.findElement(By.name("action")).click();
         }
+
+        log.logFile("Инцидент закрыт.");
     }
 
     private void searchAlert(WebDriver driver, int waitRun) throws InterruptedException {
         Thread.sleep(waitRun);
-        if (driver.findElement(By.className("list-table")).findElement(By.linkText("mail-relay1 (10.2.12.132)")).isEnabled()){
-            driver.findElement(By.linkText("Нет")).click();
+        if (driver.findElement(By.className("list-table")).findElement(By.linkText("mail-relay1")).isEnabled()){
+            List<WebElement> elements = driver.findElements(By.tagName("tr"));
+            for (int i = 0; i < elements.size(); i++){
+                if (elements.get(i).getText().indexOf("mail-relay1") != -1) {
+                    elements.get(i).findElement(By.linkText("Нет")).click();
+                    break;
+                }
+            }
         }
+
+        log.logFile("Событие найдено.");
     }
 
     private void loginZabbix(WebDriver driver, int waitRun, String login, String password) throws InterruptedException {
         Thread.sleep(waitRun);
-        if (driver.findElement(By.name("name")).isEnabled()){
+        if (driver.findElement(By.name("name")).isEnabled()) {
             driver.findElement(By.id("name")).sendKeys(login);
             driver.findElement(By.id("password")).sendKeys(password);
             driver.findElement(By.id("enter")).click();
         }
+		
+        log.logFile("Авторизация прошла успешно.");
     }
 }
