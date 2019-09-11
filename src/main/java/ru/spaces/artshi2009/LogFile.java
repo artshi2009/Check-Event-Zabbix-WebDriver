deb019dd99f7bbfbc088a282160191a0b0161896
Arthur <artshi2009@gmail.com>
2019-09-11 18:39:03 +0300

    add method pars tacktrace message

--- src/main/java/ru/spaces/artshi2009/LogFile.java
+++ src/main/java/ru/spaces/artshi2009/LogFile.java
@@ -9,12 +9,20 @@ class LogFile {
     private GregorianCalendar calendar = new GregorianCalendar();
 
     void logFile(String message) {
-        try (FileWriter writer = new FileWriter("Report.txt", true)) {
+        try (FileWriter writer = new FileWriter("Report.log", true)) {
             BufferedWriter bufferedWriter = new BufferedWriter(writer);
-            bufferedWriter.write("Время события " + calendar.getTime() + ": " + message + "\n\n");
+            bufferedWriter.write("[Время события " + calendar.getTime() + "]: " + parsLog(message));
             bufferedWriter.close();
         } catch (IOException ex){
             ex.printStackTrace();
         }
+    }
+
+    private static String parsLog(String message) {
+        StringBuilder sb = new StringBuilder(message);
+        if (sb.indexOf("Build") != -1)
+        sb.delete(sb.indexOf("For"), sb.length());
+
+        return sb + "\n";
     }
 }
