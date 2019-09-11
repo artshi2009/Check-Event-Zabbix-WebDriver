dccc4b313d4c6b8de770af1684c3f29e6df9e6f9
Arthur <artshi2009@gmail.com>
2019-09-11 18:41:01 +0300

    different corrections

--- src/main/java/ru/spaces/artshi2009/Params.java
+++ src/main/java/ru/spaces/artshi2009/Params.java
@@ -2,10 +2,10 @@ package ru.spaces.artshi2009;
 
 public class Params {
     private static int flag = 0;
-    private static String loginPage = "http://";
-    private static String dashboardPage = "http://";
-    private static String login = "";
-    private static String password = "";
+    private static String loginPage = "http://loginURL";
+    private static String dashboardPage = "http://etcURL";
+    private static String login = "username";
+    private static String password = "password";
 
 
     static String getNameButton() {
