14a0023e928038e79d28750eb50a6d4786c00d59
Arthur <artshi2009@gmail.com>
2020-01-17 18:30:16 +0300

    added a method for displaying a dialog with an exception and other fixes

--- src/main/java/ru/spaces/artshi2009/ActionWeb.java
+++ src/main/java/ru/spaces/artshi2009/ActionWeb.java
@@ -1,4 +1,4 @@
-package ru.spaces.artshi2009;
+﻿package ru.spaces.artshi2009;
 
 import com.google.common.base.Throwables;
 import io.github.bonigarcia.wdm.DriverManagerType;
@@ -6,18 +6,16 @@ import io.github.bonigarcia.wdm.FirefoxDriverManager;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+
+import javax.swing.*;
 import java.util.List;
 
 
 class ActionWeb {
     private WebDriver driver;
-    private LogFile log;
-    private int reconnectTry;
-
-    ActionWeb(){
-        log = new LogFile();
-        reconnectTry = 1;
-    }
+    private LogFile log = new LogFile();
+    private int reconnectTry = 1;
+    private String msg = "Программа не смогла закрыть инцидент в автоматическом режиме.\nЗакройте инцидент вручную!";
 
     void RunActionWeb(){
         FirefoxDriverManager.getInstance(DriverManagerType.FIREFOX).setup();
@@ -30,19 +28,19 @@ class ActionWeb {
                 driver.get(Params.getDashboardPage());
                 searchAlert(driver, 2000);
                 closingAlert(driver, 2000);
-            } catch (NoSuchElementException | InterruptedException ex) {
-                log.logFile("Не обнаружен элемент при поиске на странице: \n" +
-                        Throwables.getStackTraceAsString(ex));
-                ex.printStackTrace();
-            } catch (SessionNotCreatedException ex) {
-                log.logFile("Не удалось установить соедиение: \n" +
-                        Throwables.getStackTraceAsString(ex));
-                ex.printStackTrace();
+            } catch (NoSuchElementException | InterruptedException ex1) {
+                drawElementWarning(msg, "Не обнаружен элемент при поиске на странице: \n" +
+                        Throwables.getStackTraceAsString(ex1));
+                ex1.printStackTrace();
+            } catch (SessionNotCreatedException ex2) {
+                drawElementWarning(msg, "Не удалось установить соедиение: \n" +
+                        Throwables.getStackTraceAsString(ex2));
+                ex2.printStackTrace();
                 reconnectTry = 3;
-            } catch (WebDriverException ex) {
-                log.logFile("Непредвиденная ошибка: \n" +
-                        Throwables.getStackTraceAsString(ex));
-                ex.printStackTrace();
+            } catch (WebDriverException ex3) {
+                drawElementWarning(msg, "Непредвиденная ошибка: \n" +
+                        Throwables.getStackTraceAsString(ex3));
+                ex3.printStackTrace();
             }
         }
 
@@ -52,28 +50,28 @@ class ActionWeb {
     private void closingAlert(WebDriver driver, int waitRun) throws InterruptedException {
         Thread.sleep(waitRun);
         if (driver.findElement(By.className("table-forms-td-right")).findElement(By.tagName("textarea")).getText().isEmpty()){
-            driver.findElement(By.tagName("textarea")).sendKeys("Закрыто инженером мониторинга");
+            driver.findElement(By.tagName("textarea")).sendKeys("Closed by monitoring engineer");
             driver.findElement(By.id("close_problem")).click();
             driver.findElement(By.id("acknowledge_problem")).click();
             driver.findElement(By.name("action")).click();
         }
 
-        log.logFile("Инцидент закрыт.");
+        log.writeFile("Инцидент закрыт.");
     }
 
     private void searchAlert(WebDriver driver, int waitRun) throws InterruptedException {
         Thread.sleep(waitRun);
-        if (driver.findElement(By.className("list-table")).findElement(By.linkText("mail-relay1")).isEnabled()){
+        if (driver.findElement(By.className("list-table")).findElement(By.linkText("mail-relay1 (10.2.12.132)")).isEnabled()) {
             List<WebElement> elements = driver.findElements(By.tagName("tr"));
-            for (int i = 0; i < elements.size(); i++){
-                if (elements.get(i).getText().indexOf("mail-relay1") != -1) {
-                    elements.get(i).findElement(By.linkText("Нет")).click();
+            for(int i = 0; i < elements.size(); i++) {
+                if ((elements.get(i)).getText().indexOf("mail-relay1") != -1) {
+                    (elements.get(i)).findElement(By.linkText("Нет")).click();
                     break;
                 }
             }
         }
 
-        log.logFile("Событие найдено.");
+        log.writeFile("Событие найдено.");
     }
 
     private void loginZabbix(WebDriver driver, int waitRun, String login, String password) throws InterruptedException {
@@ -84,6 +82,13 @@ class ActionWeb {
             driver.findElement(By.id("enter")).click();
         }
 		
-        log.logFile("Авторизация прошла успешно.");
+        log.writeFile("Авторизация прошла успешно.");
+    }
+
+    public void drawElementWarning(String message, String error) {
+        JOptionPane.showMessageDialog(new JOptionPane(), message,
+                "CRITICAL ERROR! Additional information is recorded in .log-file.", JOptionPane.ERROR_MESSAGE,
+                null);
+        log.writeFile(error);
     }
 }
